b7e2310fb979a5c92bd6289451901eb61fde9aa6
infinitilover <infinitilover@gmail.com>
2025-10-24 14:54:53 -0400

    Initial Orbit deployment

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+        ],
+        unoptimized: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
+
+export default nextConfig;
+
